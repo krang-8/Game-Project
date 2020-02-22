a75c014218e4dd91f4f004d76a65905827307b23
DiljotMann <56144869+DiljotMann@users.noreply.github.com>
2020-02-21 17:22:22 -0700

    Update mainMenu.java

--- mainMenu.java
+++ mainMenu.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
 public class mainMenu {
-	private String gameName = "Untitled";
-	private String startGame = "Start Game";
-	private String settings = "Settings";
-	private String quit = "Quit";
-	
-	// Play, Settings, Quit
-	
-	public String introScreen() {
-		String printIntro = ("(1) "+ gameName + "\n" + "(2) " + startGame + "\n" + "(3) " + settings);
-		System.out.println(printIntro);
-		System.out.println("Choose a option for the game!");
-		Scanner introInputScanner = new Scanner(System.in);
-		int introInput = introInputScanner.nextInt(); 
-		while (introInput != 1 || introInput != 2 || introInput != 3) {
-			System.out.println("Invalid option, please choose again");
-			introInput = introInputScanner.nextInt();
 
-		}
-		return printIntro;
-	}
-	
-	
+    private static String gameName = "Untitled";
+    private static String startGame = "Start Game";
+    private static String settings = "Settings";
+    private static String quit = "Quit";
+
+    // Play, Settings, Quit
+
+    public String introScreen() {
+        String printIntro = ("(1) "+ gameName + "\n" + "(2) " + startGame + "\n" + "(3) " + settings);
+        System.out.println(printIntro);
+        System.out.println("Choose a option for the game!");
+        Scanner introInputScanner = new Scanner(System.in);
+        int introInput = introInputScanner.nextInt();
+
+        while (introInput != 1 && introInput != 2 && introInput != 3) {
+            System.out.println("Invalid option, please choose again");
+            introInput = introInputScanner.nextInt();
+
+        }
+        return printIntro;
+    }
+
+
 
 }
