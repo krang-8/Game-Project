025190daa364816beb713b0c016bbe835d61a103
DiljotMann <notdiljot@gmail.com>
2020-03-12 23:48:41 -0600

    Ready for Demo

--- src/Controller/ApplicationWindowController.java
+++ src/Controller/ApplicationWindowController.java
@@ -5,31 +5,53 @@ package Controller;
  */
 
 import Resources.SpriteAnimation;
+import View.swing.Platformer;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 public class ApplicationWindowController {
 
-    SpriteAnimation icon;
+
     /***************************
      * iconHolder Handler
      *
-     * Linking the ImageView to this controller
-     * <p>
      * Setting the handler to animate it using
      * class CustomAnimation
-     * <p>
+     * Fired onMouseEntered (ImageView)
+     *
      * Setting another Handler for when the mouse
-     * exits the icon holder
+     * exits the Group
+     * Fired onMouseExited (Group)
      *
      * @id iconHolder
      * @handle playAnimation()
      * @handle stopAnimation()
      ***************************/
+    SpriteAnimation icon;
+
     @FXML
     private ImageView iconHolder; //Value injected by FXMLLoader
+
+
+    @FXML
+    private void playAnimationRight() {
+        icon = new SpriteAnimation(iconHolder);
+        icon.getAnimationRight().play();
+    }
+
+    @FXML
+    private void stopAnimation() {
+        icon.getAnimationRight().stop();
+    }
+
+
     /***************************
      * menuBar Handler
      *
@@ -43,18 +65,6 @@ public class ApplicationWindowController {
     private MenuBar menuBar; //Value injected by FXMLLoader
 
     @FXML
-    private void playAnimationRight() {
-        icon = new SpriteAnimation(iconHolder);
-        icon.getAnimationRight().play();
-
-    }
-
-    @FXML
-    private void stopAnimation() {
-        icon.getAnimationRight().stop();
-    }
-
-    @FXML
     private void showMenu() {
         menuBar.setOpacity(1.0);
     }
@@ -62,6 +72,54 @@ public class ApplicationWindowController {
     @FXML
     private void hideMenu() {
         menuBar.setOpacity(0.0);
+    }
+
+
+    /***************************
+     * startButton Handler
+     *
+     * onMouseClicked
+     * @handle startGame()
+     *
+     * onMouseEntered
+     * Change the Appearance of the Background of AnchorPane
+     * Hide the menuBar and the titleTextBox
+     * @handle startButtonEntered()
+     *
+     * onMouseExited
+     * Revert Changes
+     * @handle startButtonExited()
+     ***************************/
+    @FXML
+    private Button startButton; //Value injected by FXMLLoader
+
+    @FXML
+    private AnchorPane anchorPane; //Value injected by FXMLLoader
+
+    @FXML
+    private Label titleLabel; //Value injected by FXMLLoader
+
+    @FXML
+    private void startGame(ActionEvent e) {
+        ((Stage) ((Button) e.getSource()).getScene().getWindow()).setIconified(true); // Minimize Window to Icon
+        Platformer platform = new Platformer(); //When the User wants to start the game we will hide the Javafx window, and start the Swing Platformer
+
+
+    }
+
+    @FXML
+    private void startButtonEntered() {
+        anchorPane.setStyle("-fx-background-color: radial-gradient(center 50% 50% , radius 12% , repeat, #FFFFFF, #000000, #fffaf0);");
+        menuBar.setOpacity(0.0);
+        titleLabel.setOpacity(0.0);
+
+
+    }
+
+    @FXML
+    private void startButtonExited() {
+        anchorPane.setStyle("-fx-background-color: radial-gradient(center 50% 50% , radius 12% , repeat, #fffaf0, #008080, #90ee90);");
+        titleLabel.setOpacity(1.0);
     }
 
 
